Move y and X_d scaling from the collate_fn and apply it during dataset preparation instead, and fix a bug to ensure scaling is not applied during the validation loop

chemprop/cli/train.py
@@ -1323,16 +1323,13 @@ def main(args):
 
             if "regression" in args.task_type:
                 if args.delta:
-                    train_delta_y = pd.merge(pd.DataFrame(train_dset.datasets[0]._Y),
-                                             pd.DataFrame(train_dset.datasets[0]._Y),
-                                             how='cross')
-                    train_delta_y = train_delta_y.iloc[:,1] - train_delta_y.iloc[:,0]
-                    output_scaler = StandardScaler().fit(train_delta_y.to_frame())
-                    output_scaler.mean_ = output_scaler.mean_.astype(np.float32)
-                    output_scaler.scale_ = output_scaler.scale_.astype(np.float32)
+                    output_scaler = StandardScaler()
+                    output_scaler.mean_ = np.array([0])
+                    output_scaler.scale_ = np.array([np.std(train_dset.datasets[0]._Y, ddof=0)*(2**0.5)])
+                    train_dset.normalize_targets(output_scaler)
                 else:
                     output_scaler = train_dset.normalize_targets()
-                    val_dset.normalize_targets(output_scaler)
+                val_dset.normalize_targets(output_scaler)
                 logger.info(
                     f"Train data: mean = {output_scaler.mean_} | std = {output_scaler.scale_}"
                 )
@@ -1351,17 +1348,16 @@ def main(args):
             args.num_workers,
             class_balance=args.class_balance,
             delta_dataset=args.delta,
-            output_scaler=output_scaler,
             seed=args.data_seed,
         )
         if args.class_balance:
             logger.debug(
                 f"With `--class-balance`, effective train size = {len(train_loader.sampler)}"
             )
-        val_loader = build_dataloader(val_dset, args.batch_size, args.num_workers, delta_dataset=args.delta, output_scaler=output_scaler, shuffle=False)
+        val_loader = build_dataloader(val_dset, args.batch_size, args.num_workers, delta_dataset=args.delta, shuffle=False)
         if test_dset is not None:
             test_loader = build_dataloader(
-                test_dset, args.batch_size, args.num_workers, delta_dataset=args.delta, output_scaler=output_scaler, shuffle=False
+                test_dset, args.batch_size, args.num_workers, delta_dataset=args.delta, shuffle=False
             )
         else:
             test_loader = None

chemprop/data/collate.py
@@ -8,8 +8,6 @@ from torch import Tensor
 from chemprop.data.datasets import Datum
 from chemprop.data.molgraph import MolGraph
 
-from sklearn.preprocessing import StandardScaler
-
 
 @dataclass(repr=False, eq=False, slots=True)
 class BatchMolGraph:
@@ -139,21 +137,14 @@ def collate_multicomponent(batches: Iterable[Iterable[Datum]]) -> Multicomponent
 
 
 def collate_multicomponent_delta(batches: Iterable[Iterable[Datum]],
-                                 y_scaler: StandardScaler | None=None,
-                                 X_d_scaler: StandardScaler | None=None) -> MulticomponentTrainingBatch:
+                                ) -> MulticomponentTrainingBatch:
     tbs1 = [collate_batch(batch) for batch in zip(*batches[::2])]
     tbs2 = [collate_batch(batch) for batch in zip(*batches[1::2])]
 
     delta_y = tbs2[0].Y - tbs1[0].Y
-    if y_scaler is not None:
-        delta_y = (delta_y - y_scaler.mean_)/y_scaler.scale_
-
-    if X_d_scaler is not None:
-        X_d = (torch.concat([tbs1[0].X_d, tbs2[0].X_d], dim=1) - X_d_scaler.mean_)/X_d_scaler.scale_
-    elif tbs1[0].X_d is not None:
+    X_d = None
+    if tbs1[0].X_d is not None:
         X_d = torch.concat([tbs1[0].X_d, tbs2[0].X_d], dim=1)
-    else:
-        X_d = None
 
     return MulticomponentTrainingBatch(
         [tb.bmg for tb in tbs1]+[tb.bmg for tb in tbs2],